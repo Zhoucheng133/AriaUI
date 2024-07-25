d2f3f75922a6265f175fa054b1bbd4af790568ba
周诚 <zhoucheng133@outlook.com>
2024-07-25 18:14:20 +0800

    添加主视图

---
+++ lib/main_window.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainWindow extends StatefulWidget {
+  const MainWindow({super.key});
+
+  @override
+  State<MainWindow> createState() => _MainWindowState();
+}
+
+class _MainWindowState extends State<MainWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.cyan,
+    );
+  }
+}
