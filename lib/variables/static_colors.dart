f5031464991b9600be05cf6badb18292ea14c910
周诚 <zhoucheng133@outlook.com>
2024-07-25 18:40:53 +0800

    添加静态色板

---
+++ lib/variables/static_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StaticColors{
+  Color color1=const Color.fromARGB(255, 232, 248, 245);
+  Color color2=const Color.fromARGB(255, 209, 242, 235);
+  Color color3=const Color.fromARGB(255, 163, 228, 215);
+  Color color4=const Color.fromARGB(255, 118, 215, 196);
+  Color color5=const Color.fromARGB(255, 72, 201, 176);
+  Color color6=const Color.fromARGB(255, 26, 188, 156);
+  Color color7=const Color.fromARGB(255, 23, 165, 137);
+  Color color8=const Color.fromARGB(255, 20, 143, 119);
+  Color color9=const Color.fromARGB(255, 17, 120, 100);
+  Color color10=const Color.fromARGB(255, 14, 98, 81);
+}
