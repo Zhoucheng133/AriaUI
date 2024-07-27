ee7bc50ca59de2d5998c03c83da7fc0e742bcb4a
周诚 <zhoucheng133@outlook.com>
2024-07-27 17:48:35 +0800

    添加定时器

--- lib/funcs/services.dart
+++ lib/funcs/services.dart
@@ -1,17 +1,57 @@
 
+import 'dart:async';
+
 import 'package:aria_ui/request/requests.dart';
+import 'package:aria_ui/variables/page_var.dart';
 import 'package:aria_ui/variables/task_var.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Services{
 
   final TaskVar t=Get.put(TaskVar());
+  final PageVar p=Get.put(PageVar());
+  late Timer interval;
 
-  Future<void> getActives(BuildContext context) async {
-    List lists=await Requests().getActive(context);
+  Future<void> getActives() async {
+    List lists=await Requests().getActive();
     if(lists.isNotEmpty){
       t.active.value=lists;
     }
+  }
+
+  Future<void> startService(BuildContext context) async {
+    String? version=await Requests().getVersion();
+    if(version==null){
+      if(context.mounted){
+        await showDialog(
+          context: context, 
+          builder: (context)=>ContentDialog(
+            title: Text('无法连接到Aria服务器', style: GoogleFonts.notoSansSc(),),
+            content: Text('检查RPC地址和密码是否正确!', style: GoogleFonts.notoSansSc(),),
+            actions: [
+              FilledButton(
+                child: Text('好的', style: GoogleFonts.notoSansSc(),), 
+                onPressed: (){
+                  Navigator.pop(context);
+                }
+              )
+            ],
+          )
+        );
+      }
+      return;
+    }
+    // getActives();
+    interval= Timer.periodic(const Duration(seconds: 1), (Timer time){
+      if(p.nowPage.value=='下载中'){
+        getActives();
+      }
+    });
+  }
+
+  void destoryServive(){
+
   }
 }
