5e03b92576b6f7c447553383ea124f6b7ce06434
zhoucheng <zhoucheng133@outlook.com>
2025-02-25 20:27:33 +0800

    更新获取版本号方式

--- lib/variables/setting_var.dart
+++ lib/variables/setting_var.dart
@@ -2,7 +2,6 @@ import 'package:aria_ui/variables/page_var.dart';
 import 'package:get/get.dart';
 
 class SettingVar extends GetxController{
-  String version="0.1.19";
 
   RxString rpc=''.obs;
   RxString secret=''.obs;
